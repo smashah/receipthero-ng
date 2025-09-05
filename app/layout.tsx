f14e38be298fb0b9817e744044faa1d65dec9ea8
Riccardo Giorato <riccardoemanuelegiorato@gmail.com>
2025-09-05 11:07:53 +0200

    og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   title: "Receipt Hero - Organize Your Receipts",
   description:
     "Instantly convert invoices into clear, categorized summaries. Upload your receipts and get organized spending insights.",
+  openGraph: {
+    images: "https://receipthero-self.vercel.app/og.png",
+  },
 };
 
 export default function RootLayout({
